a35b799f96693ada9a7eb5382811a10d7012c48e
Grønne <kegr.skp@edu.mercantec.dk>
2020-06-16 07:56:10 +0200

    Enemy, Player og Environment template

---
+++ 2D Top Down Shooter/Assets/Scripts/bulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+
+        } else if (collision.gameObject.tag.Contains("Enemy"))
+        {
+            collision.gameObject.GetComponent<spiderScript>().hp -= 10;
+            Destroy(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- 2D Top Down Shooter/Assets/Scripts/playerController.cs
+++ 2D Top Down Shooter/Assets/Scripts/playerController.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    Player player;
-    
+    public float moveSpeed;
+    public GameObject bullet;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Vector2 shootPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 45);
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 135);
+        } else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 225);
+        } else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 315);
+        } else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 180);
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 270);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Instantiate
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
